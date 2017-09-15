ab755883b7305d86947eee74b8ec844440938e4b
speckospock <ben.hq.stephens@gmail.com>
2017-09-15 14:59:08 -0700

    Implement static html serving

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var index = fs.readFile(archive.paths.index, 'utf8', (err, data) => {
+    if (err) {
+      console.log('sorry');
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data);
+    }
+  });
+
 };
