8de4b01dcc0b23e288d9ba8881299f192cbfeddd
speckospock <ben.hq.stephens@gmail.com>
2017-09-16 18:01:02 -0700

    Attempts to serve GET request for archived pages. Still debugging.

--- test/test.js
+++ test/test.js
@@ -30,7 +30,7 @@ describe('server', function() {
     describe('GET', function () {
       it('should return the content of a website from the archive', function (done) {
         var fixtureName = 'www.google.com';
-        var fixturePath = archive.paths.archivedSites + '/' + fixtureName;
+        var fixturePath = archive.paths.archivedSites + '/' + fixtureName + '/index.html';
 
         // Create or clear the file.
         var fd = fs.openSync(fixturePath, 'w');
--- web/request-handler.js
+++ web/request-handler.js
@@ -13,10 +13,36 @@ exports.handleRequest = function (req, res) {
   console.log(req.url);
   if (req.method === 'GET') {
     if (!(req.url === '/' || req.url === '/favicon.ico' || req.url === '/styles.css')) {
-      res.writeHead(404, {
-        'Content-Type': 'text/plain'
+      console.log('from handle request: ', req.url);
+      var newUrl = req.url.split('/')[1];
+      archive.isUrlArchived(newUrl, (archived) => {
+        if (archived) {
+          res.writeHead(200, {
+            'Content-Type': 'text/html'
+          });
+          var pathToFile = path.join(__dirname, '../archives/sites', req.url, '/index.html').toString();
+          console.log('this is checking for file\'s existence before fs.readFile: ', fs.existsSync(archive.paths.index));
+          fs.readFile(pathToFile, 'utf8', (error, data) => {
+            console.log('this is the latest path related log: ', pathToFile);
+            console.log('Does file exist??????: ', fs.existsSync(pathToFile));
+            if (error) {
+              res.writeHead(404, {
+                'Content-Type': 'text/plain'
+              });
+              console.log('error: ', error);
+              res.end('Something messed up.');
+            } else {
+              console.log('html content should be showing here: ', data);
+              res.end(data);
+            }
+          });
+        } else {
+          res.writeHead(404, {
+            'Content-Type': 'text/plain'
+          });
+          res.end('Page not found');
+        }
       });
-      res.end('Page not found');
     }
     routes.index(res);
   } else if (req.method === 'POST') {
